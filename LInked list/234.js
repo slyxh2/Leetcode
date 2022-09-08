a50a3ac20e4b7151394819f3e346fcdc75cbdc70
slyxh2 <750772121@qq.com>
2022-09-07 22:13:49 -0400

    add a better solution of 234

--- LInked list/234.js
+++ LInked list/234.js
@@ -1,12 +1,6 @@
 // 234. Palindrome Linked List
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+// use array to store the left part
 /**
  * @param {ListNode} head
  * @return {boolean}
@@ -37,3 +31,41 @@ var isPalindrome = function (head) {
     }
     return true;
 };
+
+
+// 1. reverse the right part of linkedlist 2. compare with left part
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+var isPalindrome = function (head) {
+    let slow = head,
+        fast = head;
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next || null;
+    };
+    if (fast !== null) slow = slow.next;
+    let reverse = tranvarse(slow);
+    while (reverse) {
+        if (head.val !== reverse.val) return false;
+        head = head.next;
+        reverse = reverse.next;
+    };
+    return true;
+};
+const tranvarse = (head) => {
+    let curr = head,
+        prev = null;
+    while (curr) {
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
